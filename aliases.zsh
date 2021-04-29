3b1a71dd9452fcb5015b14f73d5ed274a1610259
Tim Mortimer <tim@kiteframe.co.uk>
2021-04-29 18:21:57 +0100

    Update SSH public key name

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,4 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
