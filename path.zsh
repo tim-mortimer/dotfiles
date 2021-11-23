b912fccf3147e549ec13608ec44cc12dad224fb8
Tim Mortimer <tim@kiteframe.co.uk>
2021-11-23 21:01:21 +0000

    Add Go binaries to node_modules/.bin:vendor/bin:/Users/timmortimer/.node/bin:/Users/timmortimer/.composer/vendor/bin:node_modules/.bin:vendor/bin:/Users/timmortimer/.node/bin:/Users/timmortimer/.composer/vendor/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/opt/go/libexec/bin:/Users/timmortimer/go/bin:/usr/local/opt/go/libexec/bin:/Users/timmortimer/go/bin

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+# Load Go binaries
+export PATH="$HOME/go/bin:$PATH"
+
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
