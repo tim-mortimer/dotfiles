2ba2da9dda40ea9421c4f7b8ecc8223f5de4360c
Tim Mortimer <tim@kiteframe.co.uk>
2022-02-18 11:28:43 +0000

    Install and configure alisas for k9s

--- aliases.zsh
+++ aliases.zsh
@@ -2,3 +2,5 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias k9s="/opt/homebrew/bin/k9s --readonly"
+alias k9sadmin="/opt/homebrew/bin/k9s"
