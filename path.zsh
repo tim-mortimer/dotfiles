8e6876db45a72d92cc017f3146139dde9005b30e
Tim Mortimer <tim@kiteframe.co.uk>
2023-03-05 17:18:21 +0000

    Update Java path

--- path.zsh
+++ path.zsh
@@ -10,3 +10,4 @@ export PATH="$HOME/go/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+export JAVA_HOME=$(/usr/libexec/java_home -v 11.0.18)
