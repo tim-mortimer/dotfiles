42ba32c2117f55a40066ee2a86f95c1de4c851c6
Tim Mortimer <tim@kiteframe.co.uk>
2024-11-24 17:13:04 +0000

    Remove mysql installation tweak

--- install.sh
+++ install.sh
@@ -19,9 +19,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
 # Create code directory
 mkdir $HOME/code
 
